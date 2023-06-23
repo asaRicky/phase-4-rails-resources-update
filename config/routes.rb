2984abc345bc439e111eaaa808b0372bb470e372
asaRicky <derrickomondi208@gmail.com>
2023-06-23 09:22:38 +0300

    completed assignment

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,4 @@
 Rails.application.routes.draw do
-  resources :birds, only: [:index, :show, :create]
+  resources :birds, only: [:index, :show, :create, :update]
+  patch "/bird/:id/like", to: "bird#increment_likes"
 end
